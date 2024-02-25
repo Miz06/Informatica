3fdbc14381a9fdd6b8c4d30ce2f580fddcd61f5d
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2024-02-25 18:03:26 +0100

    .

---
+++ bancaVis2/bancaVis2/bancaVis/Banca.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancaVis
+{
+    class Banca
+    {
+        List<Conto> contiCorrenti;
+        static int id;
+        private string name;
+
+        public Banca(string _name)
+        {
+            name = _name;
+            contiCorrenti = new List<Conto>();
+        }
+
+        static public int Id
+        {
+            get { return ++id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void AggiungiConti(Conto c)
+        {
+            contiCorrenti.Add(c);
+        }
+
+        public List<Conto> CopyCorrentisti()
+        {
+            List<Conto> newCorrentisti = new List<Conto>();
+            newCorrentisti.AddRange(contiCorrenti);
+            return newCorrentisti;
+        }
+    }
+}
---
+++ bancaVis2/bancaVis2/bancaVis/Conto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancaVis
+{
+    class Conto
+    {
+        string _nome;
+        string _cognome;
+        double saldo;
+        int _id;
+
+        public Conto(int id, string nome, string cognome)
+        {
+            _id = id;
+            _nome = nome;
+            _cognome = cognome;
+            saldo = 10;
+        }
+
+        public int ID
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+        }
+
+        public string Cognome
+        {
+            get { return _cognome; }
+        }
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public int Id { get => _id; set => _id = value; }
+
+        public override string ToString()
+        {
+            return string.Format($"ID conto: {_id} | Nome: {Nome} | Cognome: {Cognome} | Saldo: {Saldo}");
+        }
+
+        public bool Movimento(double mov)
+        {
+            if (Saldo + mov < 0)
+            {
+                return false;
+            }
+            else
+            {
+                Saldo += mov;
+                return true;
+            }
+        }
+
+    }
+}
